7ba240061185e7e23e9b5dcb9dc0fff37a968b8f
AdoshSingh <adoshbhanoo@gmail.com>
2024-03-19 14:16:30 +0530

    first commit

---
+++ src/test/java/com/example/demoappfinal/DemoAppFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoappfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
